50c4361e56f402e396d0aaf63f89337dd7ff5c7d
Aleksei Kulikov <alexeyqu@yandex-team.ru>
2019-06-27 02:29:01 +0300

    one step further

--- tmp.sh
+++ tmp.sh
@@ -1,1 +1,3 @@
-git log --pretty=format:"%h %at" --author=alexeyqu  --numstat | gawk '{gsub(/(-|\w|\/)+(-|\.|\w)+\./,"")}1'
+git log --pretty=format:"%h %at" --author=alexeyqu  --numstat 
+    | gawk '{gsub(/(-|\w|\/)+(-|\.|\w)+\./,"")}1' 
+    | awk '{added[$3] += $1; deleted[$3] += $2; } END{for (i in added) print added[i], deleted[i], i}'
